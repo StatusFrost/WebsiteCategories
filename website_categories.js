01925107ff5ee4cbc420e98f14fba4e644c9c7dc
Dylan Katz <Plazmaz@users.noreply.github.com>
2016-12-25 13:53:07 -0700

    Add files via upload

---
+++ website_categories.js
@@ -0,0 +1,68 @@
+const WEBSITE_CATEGORIES = {
+    programming: [
+        "*.stackoverflow.com",
+        "*.github.com",
+        "*.github.io",
+        "*.jsfiddle.net",
+        "*.codepen.io",
+        "*.mozilla.org",
+        "*.codecademy.com"
+    ],
+    productivity: [
+        "*.slack.com",
+        "*.trello.com",
+        "*.skype.com",
+        "*.stackexchange.com",
+        "*.wikipedia.org"
+    ],
+    search: [
+        "www.google.com",
+        "google.com",
+        "*.bing.com",
+        "*.yahoo.com",
+        "*.duckduckgo.com"
+    ],
+    social: [
+        "*.twitter.com",
+        "*.facebook.com",
+        "*.instagram.com",
+        "*.snapchat.com",
+        "*.reddit.com",
+        "*.instagram.com",
+        "*.skype.com",
+        "*.youtube.com",
+        "*.steamcommunity.com",
+        "*.quora.com"
+    ],
+    gaming: [
+        "*.steampowered.com",
+        "*.discord.com",
+        "*.curseforge.com",
+        "*.gamepedia.com",
+        "*.minecraft.net",
+        "*.mojang.com",
+        "*.ign.com",
+        "*.gamefaqs.com",
+        "*.kotaku.com",
+        "*.n4g.com",
+        "*.neoseeker.com"
+    ],
+    entertainment: [
+        "*.netflix.com",
+        "*.youtube.com",
+        "*.hulu.com",
+        "*.vimeo.com"
+    ],
+    commerce: [
+        "*.paypal.com",
+        "*.upromise.com",
+        "*.chase.com",
+        "*.visa.com",
+        "*.amazon.com"
+    ],
+    adult: [
+        "*.pornhub.com",
+        "*.xhamster.com",
+        "*.xvideos.com"
+    ]
+}
